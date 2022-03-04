7518e0182f93c17ce9da12f7b7a902c10ed22f8c
Danielle Balogh <daniellegb@protonmail.com>
2022-03-04 17:23:34 -0300

    REFACTOR token, Role

--- frontweb/src/util/auth.ts
+++ frontweb/src/util/auth.ts
@@ -1,20 +1,6 @@
-import jwtDecode from 'jwt-decode';
 import { Role } from 'types/role';
-import { getAuthData } from './storage';
+import { getTokenData } from './token';
 
-export type TokenData = {
-  exp: number;
-  user_name: string;
-  authorities: Role[];
-};
-
-export const getTokenData = (): TokenData | undefined => {
-  try {
-    return jwtDecode(getAuthData().access_token) as TokenData;
-  } catch (error) {
-    return undefined;
-  }
-};
 
 export const isAuthenticated = (): boolean => {
   let tokenData = getTokenData();
